dd8d0325eaf6a6bd28176c6f5ddc41c7d0ecf602
benardhaxhijaj <benardhaxhijaj@gmail.com>
2022-12-10 18:23:42 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/*.html"
+  ],
+  theme: {
+    fontSize: {
+      ss: ['13px', '20px']
+    },
+    fontFamily: {
+      'Red Hat': ['Red Hat Display', 'system-ui']
+    }
+  },
+  plugins: [],
+}
